728805e7968c8b9ca254586ffa87a54715c6b916
Ali Haidar <alighaidar97@gmail.com>
2022-07-06 20:42:47 +0300

    Super admin + image merge

--- app/Http/Middleware/SuperAdminMiddleware.php
+++ app/Http/Middleware/SuperAdminMiddleware.php
@@ -19,7 +19,7 @@ class SuperAdminMiddleware
     public function handle(Request $request, Closure $next)
     {
         if(Auth::check()){
-            if(Auth::user()->role==1){
+            if(Auth::user()->role==0){
                 return $next($request);
             }
             else{
