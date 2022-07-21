7edd4540785f95323d50d14976ae587762ca3c13
Ali Haidar <alighaidar97@gmail.com>
2022-07-21 08:28:32 +0300

    Teams with employees relation

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -14,7 +14,7 @@ class TeamController extends Controller
      */
     public function index()
     {
-        return Team::with('project')->get();
+        return Team::with('employees','project')->get();
     }
 
     /**
